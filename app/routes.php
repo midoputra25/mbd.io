0d6e06bd9a5eb8dd785559688816dda016c53ffa
midoputra25 <vrontus1@gmail.com>
2023-10-27 09:26:51 +0800

    get by id dan post untuk setiap tabel

--- app/routes.php
+++ app/routes.php
@@ -25,7 +25,7 @@ return function (App $app) {
     $app->get('/detail', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->query('CALL selectBarang()');
+        $query = $db->query('CALL selectDetail()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
 
@@ -35,7 +35,27 @@ return function (App $app) {
     $app->get('/transaksi', function (Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
-        $query = $db->query('CALL selectBarang()');
+        $query = $db->query('CALL selectTransaksi()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/faktur', function (Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL selectFaktur()');
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/supplier', function (Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query('CALL selectSupplier()');
         $results = $query->fetchAll(PDO::FETCH_ASSOC);
         $response->getBody()->write(json_encode($results));
 
@@ -55,6 +75,54 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
+    $app->get('/detail/{kode_detail}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL selectDetailByKode(:kode_detail)');
+        $query->bindParam(':kode_detail', $args['kode_detail'], PDO::PARAM_INT);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/faktur/{nomor_faktur}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL selectFakturByKode(:nomor_faktur)');
+        $query->bindParam(':nomor_faktur', $args['nomor_faktur'], PDO::PARAM_INT);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/supplier/{kode_supplier}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL selectSupplierByKode(:kode_supplier)');
+        $query->bindParam(':kode_supplier', $args['kode_supplier'], PDO::PARAM_INT);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->get('/transaksi/{kode_transaksi}', function (Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL selectTransaksiByKode(:kode_transaksi)');
+        $query->bindParam(':kode_transaksi', $args['kode_transaksi'], PDO::PARAM_INT);
+        $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($results[0]));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
     // post data
     $app->post('/barang', function (Request $request, Response $response) {
         $parsedBody = $request->getParsedBody();
@@ -66,9 +134,8 @@ return function (App $app) {
 
         $db = $this->get(PDO::class);
 
-        $query = $db->prepare('INSERT INTO barang (kode_barang, nama_barang, harga_barang, stok_barang) values (?, ?, ?, ?)');
+        $query = $db->prepare('CALL TambahBarang(?, ?, ?, ?)');
 
-        // urutan harus sesuai dengan values
         $query->execute([$kode_barang, $nama_barang, $harga_barang, $stok_barang]);
 
         $lastId = $db->lastInsertId();
@@ -76,6 +143,103 @@ return function (App $app) {
         $response->getBody()->write(json_encode(
             [
                 'message' => 'barang disimpan dengan id ' . $lastId
+            ]
+        ));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->post('/detail', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+
+        $kode_detail = $parsedBody["kode_detail"]; 
+        $kode_barang = $parsedBody["kode_barang"];
+        $kode_transaksi = $parsedBody["kode_transaksi"];
+        $jumlah_barang = $parsedBody["jumlah_barang"];
+
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL TambahDetail(?, ?, ?, ?)');
+
+        $query->execute([$kode_detail, $kode_barang, $kode_transaksi, $jumlah_barang]);
+
+        $lastId = $db->lastInsertId();
+
+        $response->getBody()->write(json_encode(
+            [
+                'message' => 'detail disimpan dengan id ' . $lastId
+            ]
+        ));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->post('/faktur', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+
+        $nomor_faktur = $parsedBody["nomor_faktur"]; 
+        $kode_supplier = $parsedBody["kode_supplier"];
+        $tanggal_faktur = $parsedBody["tanggal_faktur"];
+        $jatuh_tempo = $parsedBody["jatuh_tempo"];
+
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL TambahFaktur(?, ?, ?, ?)');
+
+        $query->execute([$nomor_faktur, $kode_supplier, $tanggal_faktur, $jatuh_tempo]);
+
+        $lastId = $db->lastInsertId();
+
+        $response->getBody()->write(json_encode(
+            [
+                'faktur' => 'detail disimpan dengan id ' . $lastId
+            ]
+        ));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->post('/supplier', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+
+        $kode_supplier = $parsedBody["kode_supplier"]; 
+        $nama_supplier = $parsedBody["nama_supplier"];
+
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL TambahSupplier(?, ?)');
+
+        $query->execute([$kode_supplier, $nama_supplier]);
+
+        $lastId = $db->lastInsertId();
+
+        $response->getBody()->write(json_encode(
+            [
+                'message' => 'Supplier disimpan dengan id ' . $lastId
+            ]
+        ));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    $app->post('/transaksi', function (Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody();
+
+        $kode_transaksi = $parsedBody["kode_transaksi"]; 
+        $nomor_faktur = $parsedBody["nomor_faktur"];
+        $total_harga = $parsedBody["total_harga"];
+
+        $db = $this->get(PDO::class);
+
+        $query = $db->prepare('CALL TambahTransaksi(?, ?, ?)');
+
+        $query->execute([$kode_transaksi, $nomor_faktur, $total_harga]);
+
+        $lastId = $db->lastInsertId();
+
+        $response->getBody()->write(json_encode(
+            [
+                'message' => 'detail disimpan dengan id ' . $lastId
             ]
         ));
 
